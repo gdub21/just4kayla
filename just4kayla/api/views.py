4dc812f4509add82b97948edd5eea97556518d87
Gabriel Williamson <gdubbb21@mail.fresnostate.edu>
2023-02-03 11:32:39 -0800

    urls

--- just4kayla/api/views.py
+++ just4kayla/api/views.py
@@ -2,5 +2,25 @@ from django.shortcuts import render
 from django.http import JsonResponse
 # Create your views here.
 def getRoute(request):
+    routes  = [
+        {
+            'Endpoint': '/notes/date',
+            'method': 'GET',
+            'body': None,
+            'description': 'Returns notes for specific day'
+        },
+        {
+            'Endpoint': '/notes/addnew',
+            'method': 'POST',
+            'body': {'body': ""},
+            'description': 'adds new notes to db'
 
-    return JsonResponse('our api', safe=False)
+        },
+        {
+            'Endpoint': '/notes/id/deletenote',
+            'method': 'DELETE',
+            'body': None,
+            'description': 'Deletes current note'
+        }
+    ]
+    return JsonResponse(routes, safe=False)
